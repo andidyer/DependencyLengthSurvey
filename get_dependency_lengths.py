b43ffe2e19e02fe0dbe22ef2e6544033697c450f
andrewdyer <andrew.dyer@uni-saarland.de>
2023-01-10 11:29:18 +0100

    matched get_dependency_lengths.py with permute_treebanks.py

--- get_dependency_lengths.py
+++ get_dependency_lengths.py
@@ -1,26 +1,53 @@
 import argparse
+import random
 from pathlib import Path
+import logging
 
 from src.file_processor import FileProcessor
 from src.load_treebank import TreebankLoader
-from src.sentence_analyzer import SentenceAnalyzer
-from src.treebank_processor import TreebankProcessor
+from src.utils.fileutils import load_ndjson
+from src.utils.processor_factories import treebank_analyzer_factory
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
+
     required = parser.add_argument_group("required arguments")
     optional = parser.add_argument_group("optional arguments")
-    required.add_argument("--treebank", help="Treebank to load and analyse")
-    required.add_argument(
-        "--outfile",
-        type=Path,
-        help="File to output the per-sentence statistics json to",
+
+    treebank_source = required.add_mutually_exclusive_group()
+
+    treebank_source.add_argument(
+        "--treebank", type=Path, help="Treebank to load and permute"
     )
+
+    treebank_source.add_argument(
+        "--directory",
+        type=Path,
+        help="Directory from which to find treebanks by globbing",
+    )
+
     optional.add_argument(
-        "--count_root",
-        action="store_true",
-        help="Whether to count the dependency length of the root node",
+        "--glob_pattern",
+        type=str,
+        default="*",
+        help="glob pattern for recursively finding files that match the pattern",
+    )
+
+    optional.add_argument(
+        "--random_seed", type=int, default=0, help="Random seed for permutation"
+    )
+
+    output = required.add_mutually_exclusive_group()
+
+    output.add_argument(
+        "--outfile", type=Path, help="The file to output the permuted treebank(s) to"
+    )
+
+    output.add_argument(
+        "--outdir",
+        type=Path,
+        help="The directory to output the permuted treebank(s) to",
     )
 
     optional.add_argument(
@@ -42,13 +69,26 @@ def parse_args():
         default=999,
         help="Exclude sentences with more than a given maximum number of tokens",
     )
+    optional.add_argument(
+        "--count_root",
+        action="store_true",
+        help="Include the root node in the sentence analysis",
+    )
 
-    return parser.parse_args()
+    optional.add_argument("--verbose", action="store_true", help="Verbosity")
+
+    args = parser.parse_args()
+
+    return args
 
 
 def main():
     args = parse_args()
 
+    # Set random seed
+    random.seed(args.random_seed)
+
+    # Loads the remove config (for removing tokens of given type) or ignores
     if args.remove_config:
         remove_config = load_ndjson(args.remove_config)
     else:
@@ -59,17 +99,30 @@ def main():
         remove_config=remove_config, min_len=args.min_len, max_len=args.max_len
     )
 
-    # Make sentence analyzer
-    analyzer = SentenceAnalyzer(count_root=args.count_root)
+    # Make treebank processors
+    treebank_processors = []
 
-    # Make treebank processor with analyzer
-    treebank_processor = TreebankProcessor(analyzer)
+    processor = treebank_analyzer_factory(args.count_root)
+    treebank_processors.append(processor)
 
     # Make file processor
-    file_processor = FileProcessor(loader, treebank_processor)
+    file_processor = FileProcessor(loader, treebank_processors, ".ndjson")
 
-    # Process and dump to file
-    file_processor.process_file(args.treebank, args.outfile)
+    # Handle input and output
+    if args.treebank and args.outfile:
+
+        # Process and dump to file
+        file_processor.process_file(args.treebank, args.outfile)
+
+    elif args.directory and args.outdir:
+
+        # Get glob list of files and process them
+        file_processor.process_glob(args.directory, args.glob_pattern, args.outdir)
+
+    else:
+        raise argparse.ArgumentError(
+            "Incorrect or incompatible use of input and output options."
+        )
 
 
 if __name__ == "__main__":
--- src/utils/processor_factories.py
+++ src/utils/processor_factories.py
@@ -1,9 +1,17 @@
+from src.sentence_analyzer import SentenceAnalyzer
 from src.sentence_permuter import *
-from src.treebank_processor import TreebankPermuter
+from src.treebank_processor import TreebankPermuter, TreebankAnalyzer
 
+
+def sentence_analyzer_factory(count_root=False):
+    return SentenceAnalyzer(count_root=count_root)
+
+def treebank_analyzer_factory(count_root=False):
+    sentence_analyzer = sentence_analyzer_factory(count_root=count_root)
+    return TreebankAnalyzer(sentence_analyzer)
 
 def sentence_permuter_factory(mode: str, *grammar):
-    # Grammar is unpacked here, but at most these will support one.
+    # Grammar is unpacked here, but at most this will support one.
     if mode == "random_projective":
         return RandomProjectivePermuter()
     elif mode == "random_same_valency":
